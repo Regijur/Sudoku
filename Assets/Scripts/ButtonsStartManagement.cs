d0e3c3c29e4c28633fc2f2b5ca3ab71a0713a6d6
Regijur <regijur.regis@gmail.com>
2021-05-16 06:47:11 -0300

    Coding Started

--- Assets/Scripts/ButtonsStartManagement.cs
+++ Assets/Scripts/ButtonsStartManagement.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonsStartManagement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private GameObject[] ButtonsNumber;
+    private Dictionary<string, Button> BB;
+
+    private void Start()
     {
-        
+        ButtonsNumber = GameObject.FindGameObjectsWithTag("Number");
+        GameObject[] Lines = GameObject.FindGameObjectsWithTag("Lines");
+        AddToDic(Lines, BB);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //PrintButtons(Buttons);
+        }
     }
+    
+    void AddToDic(GameObject[] lines, Dictionary<string, Button> toAdd)
+    {
+        foreach (GameObject line in lines)
+        {
+            string lineName = line.name;
+            Button[] collumns = line.GetComponentsInChildren<Button>();
+            foreach(Button collumn in collumns)
+            {
+                string collumnName = collumn.name;
+                string keyName = lineName + collumnName;
+                toAdd.Add(keyName, collumn);
+            }
+        }
+    }
+    /*
+    void PrintButtons(Dictionary<string, GameObject> Dic)
+    {
+        for(int i = 0; i < Dic.Count; i++)
+        {
+            Debug.Log(Dic.Keys.ToString());
+        }
+    }*/
 }
